1612d7d26037e6e352a528d567b330f96c2eaa6d
Edward Fakier <edwardfakier@Edwards-iMac-3.attlocal.net>
2022-07-20 13:54:48 -0500

    4. Token Balances

---
+++ contracts/Token.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract Token {
+    string public name;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+}
