864efad5425b4675dd70c5a54d1b2c35d343b18e
Edward Fakier <edwardfakier@Edwards-iMac-3.attlocal.net>
2022-07-25 12:37:59 -0500

    7. Token transfer delegation

--- test/Token.js
+++ test/Token.js
@@ -126,5 +126,49 @@ describe('Token', () => {
 
  })
 
+  describe('Delegated Token Transfers', () => {
+    let amount, transaction, result
+
+    beforeEach(async () =>{
+      amount = tokens(100)
+      transaction = await token.connect(deployer).approve(exchange.address , amount)
+      result = await transaction.wait()
+    })
+
+
+    describe('Success', () => {
+      beforeEach(async () =>{
+        transaction = await token.connect(exchange).transferFrom(deployer.address , receiver.address, amount)
+        result = await transaction.wait()
+      })
+
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.be.equal(ethers.utils.parseUnits("999900", "ether"))
+        expect(await token.balanceOf(receiver.address)).to.be.equal(amount)
+      }) 
+      it('rests the allowance', async () => {
+        expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0)
+      })
+
+      it('emits a Transfer event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Transfer')
+
+        const args = event.args
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+
+    })
+
+    describe('Failure', async () => {
+      // Attempt to transfer too many tokens
+      const invalidAmount = tokens(100000000) // 100 Million, greater than total supply
+      await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted
+    })
+
+  })
+
 })
-    
+
