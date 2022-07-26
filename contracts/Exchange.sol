ec31caf6b7313527b177311f63fc5790788496c5
Edward Fakier <edwardfakier@gmail.com>
2022-07-26 16:57:04 -0500

    11. Cancel Orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
+    mapping(uint256 => bool) public orderCancelled;
 
 
 	event Deposit(
@@ -27,6 +28,16 @@ contract Exchange {
     );
     
     event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+     event Cancel(
         uint256 id,
         address user,
         address tokenGet,
@@ -123,7 +134,28 @@ contract Exchange {
             _amountGive,
             block.timestamp
         );
+    }
 
+    function cancelOrder(uint256 _id) public {
+
+        _Order storage _order = orders[_id];
+
+        require(address(_order.user) == msg.sender);
+
+        require(_order.id == _id);
+
+        orderCancelled[_id] = true;
+
+        // Emit event
+        emit Cancel(
+            _order.id,
+            msg.sender,
+            _order.tokenGet,
+            _order.amountGet,
+            _order.tokenGive,
+            _order.amountGive,
+            block.timestamp
+        );
     }
 
 }
