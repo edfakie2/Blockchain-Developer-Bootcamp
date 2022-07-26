8535bde97875b235427dbc85e7429309bc2b512f
Edward Fakier <edwardfakier@gmail.com>
2022-07-26 13:27:13 -0500

    10. Withdraw Tokens

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -10,6 +10,7 @@ contract Exchange {
 	mapping(address => mapping(address => uint256)) public tokens;
 
 	event Deposit(address token, address user, uint256 amount, uint256 balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
 
 
 	constructor(address _feeAcount, uint256 _feePercent) {
@@ -29,6 +30,17 @@ contract Exchange {
 
         // Emit an event
         emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public{
+        // Transfer tokens to user
+        Token(_token).transfer(msg.sender, _amount);
+
+        require(tokens[_token][msg.sender] >= _amount);
+
+        tokens[_token][msg.sender] = tokens[_token][msg.sender] - _amount;
+
+        emit Withdraw(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
     function balanceOf(address _token, address _user)
